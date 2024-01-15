64aaba0c2345d459b855e840b72e4ec631aaeeb6
seihantkj <143980876+seihantkj@users.noreply.github.com>
2024-01-15 09:46:59 +0700

    Add files via upload

---
+++ pertemuan 1/pertemuan ke 1/perkenalan.py
@@ -0,0 +1,6 @@
+nama = "seihan rizky widiyatno"
+kelas = "TKJ X A"
+jurusan = "TEKNIK KOMPUTER DAN JARINGAN"
+
+print("perkenalkan nama saya ",nama)
+print(" saya kelas ", kelas,"dari jurusan")
---
+++ pertemuan 1/pertemuan ke 1/variable.py
@@ -0,0 +1,18 @@
+x = 7
+y = 2
+z = "smk medikacom"
+p = False
+q = True
+
+print (x)
+print (y)
+print (z)
+print (p)
+print (q)
+
+print ("cek tipe data")
+print (type(x))
+print (type(y))
+print (type(z))
+print (type(p))
+print (type(q))
