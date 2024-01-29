e2dc980f5ee7a48512129f437f095de3e5f08bb4
seihantkj <143980876+seihantkj@users.noreply.github.com>
2024-01-29 09:30:36 +0700

    Add files via upload

---
+++ pertemuan 3/operator_aritmaika.py
@@ -0,0 +1,11 @@
+x = 5
+y = 3
+
+#penjumlahan
+print("penjumlahan 5 + 3 = ", x+y)
+print("penguranagn 5 - 3 = ",x-y)
+print("perkalian 5 * 3 = " ,x*y)
+print("pembagian 5/3 =", x/y)
+print("pembagian bulat 5 // 3", x//y)
+print("modulus 5 % 3 =" , x%y)
+print("perpangkatan 5** 3 =", x**y)
---
+++ pertemuan 3/operator_penugasan.py
@@ -0,0 +1,20 @@
+x = 10
+
+x += 3
+print("penjumlahan" ,x)
+
+x -=1
+print("penguranagn", x)
+
+x /=2
+print("pembagian", x)
+
+x *=3
+print("perkalian", x)
+
+x **= 2
+
+print("perpangkatan", x)
+
+x %= 2
+print("modulus" , x)
