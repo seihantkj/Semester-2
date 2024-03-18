4d6bd2e609494dc9eb0dc2d2e9fd357d94f1eb00
seihantkj <143980876+seihantkj@users.noreply.github.com>
2024-03-18 09:05:06 +0700

    Add files via upload

---
+++ pertemuan 8/luas lingkaran.py
@@ -0,0 +1,4 @@
+def luas_lingkaran(radius):
+    return 3.14 * radius * radius
+
+print("luas lingkaran: ", luas_lingkaran(3))
---
+++ pertemuan 8/luas_persegi.py
@@ -0,0 +1,4 @@
+def luas_persegi(sisi):
+    return sisi ** 2
+
+    print("luas persegi:  ", luas_persegi(8))
